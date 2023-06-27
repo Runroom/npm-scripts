64d59fd48c73af43653a5b1fdebc83bae9707378
Imar Abreu <abreudiazimar9620@gmail.com>
2023-06-27 11:55:55 +0100

    Clean next eslint

--- config/eslint/extends/next.js
+++ config/eslint/extends/next.js
@@ -13,20 +13,6 @@ module.exports = {
     'react/react-in-jsx-scope': 'off',
     'import/no-anonymous-default-export': 'error',
     'react-hooks/exhaustive-deps': 'error',
-    'react/display-name': 2,
-
-    'curly': 0,
-    'lines-around-comment': 0,
-    'max-len': 0,
-    'no-confusing-arrow': 0,
-    'no-mixed-operators': 0,
-    'no-tabs': 0,
-    'no-unexpected-multiline': 0,
-    'quotes': 0,
-    '@typescript-eslint/lines-around-comment': 0,
-    '@typescript-eslint/quotes': 0,
-    'babel/quotes': 0,
-    'vue/html-self-closing': 0,
-    'vue/max-len': 0
+    'react/display-name': 2
   }
 };
