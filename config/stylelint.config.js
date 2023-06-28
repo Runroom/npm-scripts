44ca9ad53c15ced5957ed75362625fc3a556506f
Imar Abreu <abreudiazimar9620@gmail.com>
2023-06-28 09:12:30 +0100

    Remove the deprecated package stylelint-config-prettier (#31)

--- config/stylelint.config.js
+++ config/stylelint.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['stylelint-config-recommended', 'stylelint-config-prettier'],
+  extends: ['stylelint-config-recommended', 'stylelint-config-standard'],
   rules: {
     'at-rule-no-unknown': [
       true,
