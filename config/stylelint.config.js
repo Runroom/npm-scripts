c0170a3ff85811ce2fbcb2d6b51b7bb14b8067a1
Imar Abreu <imar@runroom.com>
2023-11-09 16:09:29 +0000

    Fix stylelint selector-class-pattern (#38)

--- config/stylelint.config.js
+++ config/stylelint.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  'extends': ['stylelint-config-recommended', 'stylelint-config-standard'],
-  'rules': {
+  extends: ['stylelint-config-recommended', 'stylelint-config-standard'],
+  rules: {
     'at-rule-no-unknown': [
       true,
       {
@@ -22,15 +22,15 @@ module.exports = {
           'error'
         ]
       }
-    ]
-  },
-  'selector-class-pattern': [
-    '^[a-z]([-]?[a-z0-9]+)*((__[a-z0-9]([-]?[a-z0-9]+)*)?(--[a-z0-9]([-]?[a-z0-9]+)*)?)*$',
-    {
-      resolveNestedSelectors: true,
-      message: function expected(selectorValue) {
-        return `Expected class selector "${selectorValue}" to match BEM CSS pattern https://en.bem.info/methodology/css. Selector validation tool: https://regexr.com/3apms`;
+    ],
+    'selector-class-pattern': [
+      '^[a-z]([-]?[a-z0-9]+)*((__[a-z0-9]([-]?[a-z0-9]+)*)?(--[a-z0-9]([-]?[a-z0-9]+)*)?)*$',
+      {
+        resolveNestedSelectors: true,
+        message: function expected(selectorValue) {
+          return `Expected class selector "${selectorValue}" to match BEM CSS pattern https://en.bem.info/methodology/css. Selector validation tool: https://regexr.com/3apms`;
+        }
       }
-    }
-  ]
+    ]
+  }
 };
